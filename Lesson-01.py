0572047bc79fab6f117069475ff104331de6bd4f
Purple Note <purplenote16@gmail.com>
2020-08-30 12:31:58 -0500

    Lesson 01

--- Lesson-01.py
+++ Lesson-01.py
@@ -1,10 +1,24 @@
 # Python Code Lesson-01
 # You can find the source code at Github:
-# https://github.com/purplenote/PythonLessons
+# https://github.com/purplenote/PythonLesson
 
+# int float string
+
+print("Hello World")
 
 a = 1
 b = 0.5
-c = a + b
-print(c)
-print(type(c))
+c = "Python"
+
+print(a)
+
+print(type(a))
+
+rate = 1e-3
+print(type(rate))
+
+d = a + b
+print(d)
+print("Type of d:", type(d))
+
+print("Thank you!")
