f23118798ea31563d717e8819bcf23245c23ac42
Purple Note <purplenote16@gmail.com>
2020-08-30 20:53:14 -0500

    Lesson2

---
+++ Lesson-02.py
@@ -0,0 +1,25 @@
+# Python Code Lesson-02
+# You can find the source code at Github:
+# https://github.com/purplenote/PythonLesson
+
+# List
+
+a = [1, 2, 7, 3, 4, 6]
+print(a)
+a[3] = a[3] + 1
+a[4] = a[4] * 2
+
+a.append(8)
+a.remove(7)
+a.pop(4)
+a.sort()
+a.reverse()
+
+
+b = ["Fish", "Dog"]
+c = a + b
+d = list(range(10))
+
+print("Thank you!")
+
+
